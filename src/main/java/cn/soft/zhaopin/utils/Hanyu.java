81a8adeac3ea08bf7e73be78e41f7b18fd4cad2e
15779163581@163.com <15779163581@163.com>
2018-09-04 09:16:11 +0800

    完善

--- src/main/java/cn/soft/zhaopin/utils/Hanyu.java
+++ src/main/java/cn/soft/zhaopin/utils/Hanyu.java
@@ -5,29 +5,29 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
 import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
+/**
+ * 汉语转拼音工具类
+ *
+ * @author NetWork
+ */
 public class Hanyu {
 
-    private HanyuPinyinOutputFormat format = null;
-
+    private HanyuPinyinOutputFormat format;
     private String[] pinyin;
 
-
     public Hanyu() {
-
         format = new HanyuPinyinOutputFormat();
-
         format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-
-
         pinyin = null;
-
     }
 
-
-//转换单个字符
-
+    /**
+     * 转换单个字符
+     *
+     * @param c 单个字符
+     * @return String
+     */
     public String getCharacterPinYin(char c) {
-
         try {
             System.out.println(c);
             pinyin = PinyinHelper.toHanyuPinyinStringArray(c, format);
@@ -40,12 +40,15 @@ public class Hanyu {
         }
         // 只取一个发音，如果是多音字，仅取第一个发音
         return pinyin[0];
-
     }
 
 
-//转换一个字符串
-
+    /**
+     * 转换一个字符串
+     *
+     * @param str 需要转换的字符串
+     * @return 转换后的字符串
+     */
     public String getStringPinYin(String str) {
         StringBuilder sb = new StringBuilder();
         String tempPinyin = null;
@@ -59,7 +62,5 @@ public class Hanyu {
             }
         }
         return sb.toString();
-
     }
-
 }
